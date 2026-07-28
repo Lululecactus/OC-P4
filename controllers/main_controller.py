40734e40f602f685ed28ef622b2b160e7413d461
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2026-07-28 19:07:45 +0200

    Add player submenu in MainController, restructure navigation

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -7,11 +7,13 @@ from controllers.player_controller import PlayerController
 from controllers.tournament_controller import TournamentController
 from controllers.report_controller import ReportController
 from constants import (
-    MAIN_ADD_PLAYER,
-    MAIN_LIST_PLAYERS,
+    MAIN_PLAYERS,
     MAIN_TOURNAMENTS,
     MAIN_REPORTS,
     MAIN_QUIT,
+    PLAYER_ADD,
+    PLAYER_LIST,
+    PLAYER_BACK,
 )
 
 
@@ -21,27 +23,41 @@ class MainController:
         players = load_players()
         players_dict = {p.chess_id: p for p in players}
 
-        menu_view = MenuView()
+        self.menu_view = MenuView()
         player_view = PlayerView()
         tournament_view = TournamentView()
         report_view = ReportView()
 
-        self.menu_view = menu_view
         self.player_controller = PlayerController(players, player_view)
         self.tournament_controller = TournamentController(
             players_dict, tournament_view
         )
         self.report_controller = ReportController(players_dict, report_view)
 
+    def _player_menu(self):
+        while True:
+            choice = self.menu_view.show_player_menu()
+
+            if choice == PLAYER_ADD:
+                self.player_controller.create_player()
+                self.menu_view.wait_for_enter()
+
+            elif choice == PLAYER_LIST:
+                self.player_controller.list_players()
+                self.menu_view.wait_for_enter()
+
+            elif choice == PLAYER_BACK:
+                break
+
+            else:
+                self.menu_view.show_message("Choix invalide.")
+
     def run(self):
         while True:
             choice = self.menu_view.show_main_menu()
 
-            if choice == MAIN_ADD_PLAYER:
-                self.player_controller.create_player()
-
-            elif choice == MAIN_LIST_PLAYERS:
-                self.player_controller.list_players()
+            if choice == MAIN_PLAYERS:
+                self._player_menu()
 
             elif choice == MAIN_TOURNAMENTS:
                 self.tournament_controller.tournament_menu()
